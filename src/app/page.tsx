bdd3a8e939fa0384f072b7ce69d52a578246f260
rich <rickygb082002@gmail.com>
2026-01-19 15:03:23 -0600

    comentando la funcionalidad del efecto de escritura en mi parrafo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,23 @@
 import { useEffect, useState } from "react";
 
 function HomePage() {
+  // Variables
   const text =
-    "Personal portfolio to showcase my projects, skills, and experience as a developer.";
-  const typingSpeed = 90;
-  const deletingSpeed = 60;
-  const pauseAfterTyping = 1200;
-  const pauseBeforeRestart = 500;
-  const [displayedText, setDisplayedText] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [index, setIndex] = useState(0);
+    "Personal portfolio to showcase my projects, skills, and experience as a developer."; //Este es el texto que se va a escribir letra por letra
+  const typingSpeed = 90; // Velocidad de escritura en pantalla
+  const deletingSpeed = 60; // Velocidad en la cual se borrara todo el texto en pantalla
+  const pauseAfterTyping = 1200; // Tiempo que se deja en pantalla antes de borrar
+  const pauseBeforeRestart = 500; // Tiempo para volver a escribir desde 0
+
+  // States
+  const [displayedText, setDisplayedText] = useState(""); // Mostrar el texto en pantalla
+  const [isDeleting, setIsDeleting] = useState(false); // Condicional, para saber cuando esta escribiendo o no
+  const [index, setIndex] = useState(0); // Posicion actual dentro del texto i
 
   useEffect(() => {
-    let timeout: NodeJS.Timeout;
+    let timeout: NodeJS.Timeout; // para limpiar
 
-    if (!isDeleting && index < text.length) {
+    if (!isDeleting && index < text.length) { // si no estamos borrando y tampoco no llegamos al final del texto : escribimos
       // TYPING
       timeout = setTimeout(() => {
         setDisplayedText((prev) => prev + text[index]);
