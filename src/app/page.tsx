9e193c61ae94cc45ce0c6aa5b7c6f1722ed9ec12
rich <rickygb082002@gmail.com>
2026-01-19 00:31:37 -0600

    firts steps

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <main className="relative">
+      <div>
+        <h1><h1>My Portfolio!.</h1></h1>
+      </div>
+    </main>
+  )
+}
+
+export default HomePage
