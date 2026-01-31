e90a27f56affb368029302e09e72b0dc983b9008
rich <rickygb082002@gmail.com>
2026-01-30 19:15:47 -0600

    backdrop-blur-lg 30%

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-black/60 backdrop-blur-lg shadow-md">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-black/30 backdrop-blur-lg shadow-md">
       <div className="mx-auto flex justify-between items-center py-3 px-6">
         <h3 className="font-bold text-white text-3xl mx-6">
           <a href={"/"}> &lt;Rich Portfolio/&gt;</a>
