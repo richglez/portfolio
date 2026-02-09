26902926ff733943fbb580ff30bec6f986b07bee
rich <rickygb082002@gmail.com>
2026-02-09 16:32:55 -0600

    overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#050505ff] bg-mesh relative overflow-x-hidden`}
       >
